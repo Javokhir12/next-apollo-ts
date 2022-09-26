1423fd8c58903b1d2abdfb36bafcf64e08b5fef2
Javokhir Rajabboev <javokhir_rajabboev@epam.com>
2022-09-26 19:09:39 +0500

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/graphql-client';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ initialApolloState: any }>) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
